a0405321a6425561ba707e45300fff5725a38b8b
xuanthuy78 <phamthixuanthuy78@gmail.com>
2019-05-22 11:25:49 +0700

    xong

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+            <div className="card">
+              <div className="card-header bg-primary">Color Picker</div>
+              <div className="card-body">Content</div>
+            </div>
+          </div>
+          <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+            <div className="card">
+              <div className="card-header bg-warning">Size: 15px</div>
+              <div className="card-body">Content</div>
+            </div>
+            <button type="button" className="btn btn-primary mt-2">
+              Reset
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+            <p>Color: red - Fontsize: 15px</p>
+            <div id="content">Nội dung setting</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/ColorPicker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ColorPicker extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ColorPicker
