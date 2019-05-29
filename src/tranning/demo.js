19fb9d88cebed432e2eee97d8b7e68946cf03190
xuanthuy78 <phamthixuanthuy78@gmail.com>
2019-05-29 11:08:24 +0700

    hieu them redux

--- src/tranning/demo.js
+++ src/tranning/demo.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux'
-//2. bien khoi tao ban dau
+//2. bien khoi tao ban dau, những state ban đầu, chức năng cần những biến gì
 var initialState = {
     status : false,
     sort: {
@@ -8,10 +8,12 @@ var initialState = {
     }
 }
 // 3.Tao reducer ben trong co 2 bien state, action
+// sate luc ngày đang mang giá trị mặc định vì state = initialState, mục đích của reducer là trả ra 1 state mới
 var myReducer = (state = initialState, action) => {
     //6. Phan tích
     if(action.type === 'TOGGLE_STATUS'){
         state.status = !state.status
+        //reaturn state để trả ra state mới
         return state;
     }
 
@@ -31,13 +33,13 @@ var myReducer = (state = initialState, action) => {
         //     by: action.sort.by,
         //     value: action.sort.value
         // }
-        console.log('adfdf', action)
     }
 
     return state;
 }
 // 1.Khai tao store, phai co reduce truyuen vào
 const store = createStore(myReducer);
+console.log(store);
 console.log("default", store.getState());
 // 4. Tao action là obj, type ten action, action ko có tham sso
 var action = { type: "TOGGLE_STATUS"}; 
